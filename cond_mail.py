0eb6a514efa011dff210455221a60ef7f5891aa7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-10 08:27:08 +0100

    Update cond_mail.py v3.2.4

--- cond_mail.py
+++ cond_mail.py
@@ -104,7 +104,7 @@ def main():
 
     # Process argument list from command line.
     args_array = arg_parser.arg_parse2(sys.argv, opt_val_list,
-                                       multi_val=opt_multi_list) # NOSONAR
+                                       multi_val=opt_multi_list) #NOSONAR
 
     if not gen_libs.help_func(args_array, __version__, help_message) \
        and not arg_parser.arg_require(args_array, opt_req_list):
